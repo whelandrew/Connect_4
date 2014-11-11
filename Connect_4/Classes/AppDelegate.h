7d40d5a8687accf0bfe5b72fab1762fa49cc8f68
whelandrew <Whelandrew@gmail.com>
2014-11-11 13:09:12 -0800

    Initial Commit

---
+++ Connect_4/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Connect_4
+//
+//  Created by Krom on 11/11/14.
+//  Copyright SomethingOrOther 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Connect_4/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Connect_4
+//
+//  Created by Krom on 11/11/14.
+//  Copyright SomethingOrOther 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
